14e4147cecacbbaa635a5631755376945291c74b
datacenterUCM <131399237+datacenterUCM@users.noreply.github.com>
2023-05-30 19:35:08 +0200

    Delete influx_module directory

--- influx_module/index.js
+++
@@ -1,8 +0,0 @@
-const { ConfigParams } = require("./ConfigParams");
-const { MqttModule } = require("./Mqtt");
-const { LogicImpl } = require("./LogicImpl");
-
-logicImpl = new LogicImpl()
-
-mqtt = new MqttModule( logicImpl )
-
