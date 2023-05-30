d3d29398bb6f4104218ae434b7df5ab7475b1325
datacenterUCM <131399237+datacenterUCM@users.noreply.github.com>
2023-05-30 19:30:13 +0200

    Delete verge3D_vue_app directory

--- verge3D_vue_app/dist/js/about.b848f908.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmy_app"]=self["webpackChunkmy_app"]||[]).push([[443],{7750:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var s=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],e=u(1001),l={},r=(0,e.Z)(l,s,a,!1,null,null,null),i=r.exports}}]);
-//# sourceMappingURL=about.b848f908.js.map
