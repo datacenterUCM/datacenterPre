5cab1d39474c48af4ad4b7ab42ca6bf9d8f8df32
Sergio <sergiomunozgarrido1@gmail.com>
2023-06-13 18:09:05 +0200

    secrets

--- README.txt
+++ README.txt
@@ -1,32 +1,6 @@
 Se ha creado una cuenta de google para el centro de datos. 
 Dicha cuenta es para tener acceso al github del proyecto.
 
-CUENTA DE GOOGLE:
-Correo: datacenterUCM@gmail.com
-Contraseña: IoTwins1999
-
-CUENTA DE GITHUB:
-Nombre de usuario: datacenterUCM
-Contraseña: digitaltwin23
-
-DITTO:
-Nombre: datacenter
-Contraseña: esp32twin
-
-GRAFANA
-Nombre: datacenter
-Contraseña: IoTchart1999
-
-RASPI:
-IP: 147.96.81.123
-Usuario: datacenter
-Contraseña: 3x3=nueve
-
-MAQUINA VIRTUAL:
-IP: 147.96.85.89
-Usuario: timeuser
-Contraseña: T3m2D1t1$
-
 Para clonar el repositorio de github es necesario usar el siguiente comando:
 git clone git@github.com:datacenterUCM/datacenter.git
 
--- python_server_blender/ConfigParams.py
+++ python_server_blender/ConfigParams.py
@@ -1,6 +1,10 @@
+from Secrets import Secrets
+
 class ConfigParams:
 
     def __init__(self):
+
+        self.secrets = Secrets()
 
         self.IP = '0.0.0.0' #Dirección de bind para el contenedor de docker
         #self.IP = '192.168.1.41'
@@ -12,15 +16,13 @@ class ConfigParams:
         #self.dittoUrl = 'http://192.168.1.41:8080/api/2/things/org.eclipse.ditto:sergio-room-v2'
         #self.dittoUrl = 'http://81.34.233.47:8080/api/2/things/org.eclipse.ditto:sergio-room-v2'
 
-        self.dittoUser = 'datacenter'
-        #self.dittoUser = 'sergio'
-        self.dittoPass = 'esp32twin'
-        #self.dittoPass = 'sergiotfm'
-
         # Longitud de los lados de la sala
         self.sideXLength = 3 * 7.26
         self.sideYLength = 3 * 2.74
         self.sideZLength = 3 * 4.5
+
+        self.dittoUser = self.secrets.dittoUser
+        self.dittoPass = self.secrets.dittoPass
 
         #Medidas del gemelo digital
         self.twinXLength = 7.26
