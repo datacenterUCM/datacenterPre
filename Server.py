c9376952abf3754138f25dc6862526013ec9adf0
Sergio <sergiomunozgarrido1@gmail.com>
2023-06-14 17:00:39 +0200

    a

--- Server.py
+++
@@ -1,7 +0,0 @@
-class Secrets:
-
-    def __init__(self):
-        self.dittoUser = 'datacenter'
-        #self.dittoUser = 'sergio'
-        self.dittoPass = 'esp32twin'
-        #self.dittoPass = 'sergiotfm'
