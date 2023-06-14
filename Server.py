bcef8a0b83bbddbe9b67394ea038b21286f2a00b
Sergio Munoz Garrido <datacenterUCM@gmail.com>
2023-06-14 18:23:41 +0200

    a

---
+++ Server.py
@@ -0,0 +1,7 @@
+class Secrets:
+
+    def __init__(self):
+        self.dittoUser = 'datacenter'
+        #self.dittoUser = 'sergio'
+        self.dittoPass = 'esp32twin'
+        #self.dittoPass = 'sergiotfm'
